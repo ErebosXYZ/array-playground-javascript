8db584b4df6e589e0e3385ccd3bddc91687d01bd
ErebosXYZ <robertmaynou@gmail.com>
2025-03-17 22:03:51 +0100

    ex6a done

--- ex6.js
+++ ex6.js
@@ -18,6 +18,12 @@ let pezBuscado = "Estrella";
 let pezEncontrado = false;
 let i = 0;
 
+// pezEncontrado és "false".
+// El bucle s'executarà mentre pezEncontrado sigui fals, i "i" estigui dins de l'array (posicions 0 a 3). Si pezEncontrado és "true", el bucle s'aturarà. També s'aturarà quan "i" arribi a listaPeces.length -1 (3).
+// listaPeces.length = 4 
+// -1 serien 3
+// Si i és més petit de 3, el bucle no recorrerà l'array fins a "Estrella" i per tant la resposta serà false perquè el bucle haurà recorregut l'array dels índex 0 a 2
+
 while (!pezEncontrado && i < listaPeces.length - 1) {
   if (listaPeces[i] == pezBuscado) {
     pezEncontrado = true;
--- ex6a.js
+++ ex6a.js
@@ -3,15 +3,19 @@
  * 
  * En Mario Party, cuando un jugador gana una estrella, grita su nombre con emoción.
  * Dado un array de nombres de jugadores, devuelve un nuevo array donde todos los nombres están en mayúsculas.
+ * 
+ * shoutPlayerNames recorre l'array players i en converteix cada string a majúscules
  */
 
 function shoutPlayerNames(players) {
     // RECUERDA: PRIMERO escribe lo que hay que hacer con tus palabras. Luego, viene el código
     let arrayUpperCase = [];
-
+    // la variable arrayUpperCase es mostra entre []
     // Codificar aquí....
-
+    arrayUpperCase = players.map((players) => players.toUpperCase());
+    // perquè arrayUpperCase retorni els arrays en majúscules, utilitzem el mètode map que serveix per retornar arrays nous a partir dels elements de l'array original. En aquest cas, players és substituït per players en majúscules
     return arrayUpperCase;
+    // retornem la variable resultant
 
 }
 
